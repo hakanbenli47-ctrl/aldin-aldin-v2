a6c6190349257fa0a392b3a9c58f9127831b3b3b
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2025-08-25 14:18:48 +0300

    destekentegre

---
+++ pages/destek-personel.tsx
@@ -0,0 +1,8 @@
+export default function DestekPersonel() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>📞 Destek Personeli Paneli</h1>
+      <p>Buradan destek taleplerini görebileceksiniz.</p>
+    </div>
+  );
+}
