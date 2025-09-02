8f7db823f4a7acf14c96c35a7d99dd9036bafe6b
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2025-09-02 18:22:52 +0300

    icondeğşiklik

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,11 +17,27 @@ export default function Document() {
         <meta name="apple-mobile-web-app-title" content="80bir" />
         <meta name="format-detection" content="telephone=no" />
 
+        {/* Favicon (Google için) */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
         {/* İkonlar (manifest ile eşleşen dosyalar) */}
         <link rel="icon" href="/icons/icon-192.png" sizes="192x192" />
         <link rel="icon" href="/icons/icon-512.png" sizes="512x512" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <link rel="mask-icon" href="/icons/icon-512.png" color="#2563eb" />
+
+        {/* Structured Data: Organization Logo */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": "https://www.80bir.com.tr",
+              "logo": "https://www.80bir.com.tr/favicon.png",
+            }),
+          }}
+        />
       </Head>
       <body>
         <Main />
