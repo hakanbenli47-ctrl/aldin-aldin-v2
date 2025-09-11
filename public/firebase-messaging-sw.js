690619b037ab2632cb8277de2418b8e70532b00c
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2025-09-11 17:21:28 +0300

    güncelleme003

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,17 +1,18 @@
 // public/firebase-messaging-sw.js
+
 /* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js');
 
-// Buradaki değerler PUBLIC'tır; env kullanamazsın. NEXT_PUBLIC olanları buraya MANUEL yaz.
+// GERÇEK ANAHTARLARINIZ BURADA
 firebase.initializeApp({
-  apiKey: "NEXT_PUBLIC_FIREBASE_API_KEY_DEĞERİN",
-  authDomain: "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_DEĞERİN",
-  projectId: "NEXT_PUBLIC_FIREBASE_PROJECT_ID_DEĞERİN",
-  storageBucket: "NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_DEĞERİN",
-  messagingSenderId: "NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_DEĞERİN",
-  appId: "NEXT_PUBLIC_FIREBASE_APP_ID_DEĞERİN",
-  measurementId: "NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID_DEĞERİN",
+  apiKey: "AIzaSyBvd_VoZsK6WeoAQulITwp9Q6c5FlbHSM",
+  authDomain: "birapp-44f8a.firebaseapp.com",
+  projectId: "birapp-44f8a",
+  storageBucket: "birapp-44f8a.appspot.com",
+  messagingSenderId: "987619236912",
+  appId: "1:987619236912:web:b71a7398523e5590221f5",
+  measurementId: "G-BTLZMP04HS",
 });
 
 const messaging = firebase.messaging();
