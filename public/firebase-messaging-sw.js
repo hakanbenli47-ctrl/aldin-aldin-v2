e9e8188cb56c0c82e3b498d05a290982f6d352d0
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2025-09-12 17:30:16 +0300

    güncelleme

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAz_ZEDKke3Pg0SyA2uV5jIOuu5pvRuKRM",
-  authDomain: "birapp-67573.firebaseapp.com",
-  projectId: "birapp-67573",
-  storageBucket: "birapp-67573.appspot.com", // ✅ düzeltildi
-  messagingSenderId: "575930274632",
-  appId: "1:575930274632:web:760add3bcb2558e8d0d97d",
-  measurementId: "G-SR16YENH0L"
+ apiKey: "AIzaSyBUsd_vOzSK6WE0AQuQlIwDgOESf10bHSM",
+  authDomain: "birapp-44f8a.firebaseapp.com",
+  projectId: "birapp-44f8a",
+  storageBucket: "birapp-44f8a.firebasestorage.app",
+  messagingSenderId: "987619236912",
+  appId: "1:987619236912:web:7b1a7398523e55090221f5",
+  measurementId: "G-BTLZMP04HS"
 });
 
 const messaging = firebase.messaging();
