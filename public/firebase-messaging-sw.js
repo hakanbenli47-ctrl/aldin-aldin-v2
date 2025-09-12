5ebda4478feb98fc902b2ce1681d3476db0505cf
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2025-09-12 14:04:20 +0300

    güncelleme013

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,16 @@
-// public/firebase-messaging-sw.js
-
 /* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js');
 
-// GERÇEK ANAHTARLARINIZ BURADA
+// ✅ Güncel 67573 projenin config değerleri
 firebase.initializeApp({
-  apiKey: "AIzaSyBvd_VoZsK6WeoAQulITwp9Q6c5FlbHSM",
-  authDomain: "birapp-44f8a.firebaseapp.com",
-  projectId: "birapp-44f8a",
-  storageBucket: "birapp-44f8a.appspot.com",
-  messagingSenderId: "987619236912",
-  appId: "1:987619236912:web:b71a7398523e5590221f5",
-  measurementId: "G-BTLZMP04HS",
+  apiKey: "AIzaSyAz_ZEDKke3Pg0SyA2uV5jIOuu5pvRuKRM",
+  authDomain: "birapp-67573.firebaseapp.com",
+  projectId: "birapp-67573",
+  storageBucket: "birapp-67573.appspot.com",
+  messagingSenderId: "575930274632",
+  appId: "1:575930274632:web:76dadd3bcb2558e8d0d97d",
+  measurementId: "G-SR16YENH0L",
 });
 
 const messaging = firebase.messaging();
@@ -29,10 +27,9 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(title, options);
 });
 
-// Bildirime tıklama davranışı (isteğe bağlı)
+// Bildirime tıklama davranışı
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   const url = (event.notification.data && event.notification.data.url) || "/";
   event.waitUntil(clients.openWindow(url));
-
 });
